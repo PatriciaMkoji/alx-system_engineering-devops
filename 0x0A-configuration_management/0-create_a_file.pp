d925af93f587974f001d61fabd6d0f079e4e351c
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-23 05:04:34 +0300

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a File
+file {'/tmp/school':
+ensure  => file,
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+mode    => '0744',
+}

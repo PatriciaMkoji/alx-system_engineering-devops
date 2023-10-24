01e9c06c5c6a04adf9c1b8d0461ccaa3f61715d5
PatriciaMkoji <keyshotrisha@gmail.com>
2023-10-23 19:48:47 -0700

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,12 +9,12 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
     to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-    
+
     done_tasks = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
+                  title.get('completed') is True]
 
     print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
                                                           len(done_tasks),
                                                           len(to_do)))
-    
+
     [print("\t {}".format(title)) for title in done_tasks]
